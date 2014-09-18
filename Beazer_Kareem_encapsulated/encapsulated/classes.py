3faa58bf07f320bad983e3e0e82f2c3278b5c917
kbeazer <kbeazer@fullsail.edu>
2014-09-18 12:22:25 -0400

    encapsulated file structure organized

--- Beazer_Kareem_encapsulated/encapsulated/classes.py
+++ Beazer_Kareem_encapsulated/encapsulated/classes.py
@@ -1,1 +1,28 @@
-__author__ = 'KBmac'
+class HomePage(object):
+    def __init__(self):
+        self.title = "Gamers World"
+        self.css = "css/style.css"
+        self.head =  """
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>{self.title}</title>
+        <link href="{self.css}" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        """
+
+        self.body = """
+
+
+        """
+        self.close = """
+    </body>
+</html>
+        """
+
+    def print_out(self):
+        all = self.head + self.body + self.close
+        all = all.format(**locals())
+        return all
+
