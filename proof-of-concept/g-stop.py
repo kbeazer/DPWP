b58cf37beb28a40933c3b58000c8e30a1c8e8ab7
kbeazer <kbeazer@fullsail.edu>
2014-09-20 16:59:23 -0400

    proof of concept added to repo

---
+++ proof-of-concept/g-stop.py
@@ -0,0 +1,13 @@
+class GameGrab(object):
+    def __init__(self):
+        self.title = "Game Grabber"
+        self.css = "css/style.css"
+        selft.head = """
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title></title>
+                <link href="{self.css}" rel="stylesheet" type="css/text" />
+            </head>
+
+        """
