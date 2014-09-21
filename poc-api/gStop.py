77cfa041e8e4201adcc742d10d97b200e4c81e2d
kbeazer <kbeazer@fullsail.edu>
2014-09-20 21:33:46 -0400

    format.locals added to gStop.py

---
+++ poc-api/gStop.py
@@ -0,0 +1,59 @@
+class GameGrab(object):
+    def __init__(self):
+        self.title = "Game Grabber"
+        self.css = "css/style.css"
+        self.head = """
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>{self.title}</title>
+        <link href="{self.css}" rel="stylesheet" type="css/text" />
+    </head>
+    <body>
+        """
+        self.body = """
+        <h1>The Video Game Grabber</h1>
+        <h2>The one stop center for all your gaming needs.</h2>
+        <form method="GET">
+            <input type="text" name="user" />
+            <input type="submit" value="Search" name="entry" />
+        </form>
+        """
+        self.close = """
+    </body>
+</html>
+        """
+
+    def print_page(self):
+        page = self.head + self.body + self.close
+        page = page.format(**locals())
+        return page
+
+
+class ResultsView(object):
+    def __init__(self):
+        self.title = "Game Grabber"
+        self.css = "css/style.css"
+        self.head = """
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>{self.title}</title>
+        <link href="{self.css}" rel="stylesheet" type="css/text" />
+    </head>
+    <body>
+        """
+        self.body = """
+       <form method="GET">
+            <input type="submit" value="View the Results" name="results" />
+        </form>
+        """
+        self.close = """
+    </body>
+</html>
+        """
+
+    def print_page(self):
+        page = self.head + self.body + self.close
+        page = page.format(**locals())
+        return page
--- poc-api/main.py
+++ poc-api/main.py
@@ -1,24 +1,35 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+"""
+Kareem K. Beazer
+9/20/14
+Design Patterns for Web Programming - Online
+POC
+"""
+
 import webapp2
+from gStop import GameGrab
+from gStop import ResultsView
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        g = GameGrab()
+        r = ResultsView()
+        self.response.write(g.print_page())
+        results = self.request.get('results')
+        view = self.request.get('user')
+
+        if view == "":
+            self.response.write('Please enter a search query to continue.')
+        else:
+            self.response.write('You just searched for: ' + view)
+            self.response.write(r.print_page())
+
+        if results:
+            self.response.clear()
+            self.response.write('I work!!!!')
+        else:
+            pass
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
