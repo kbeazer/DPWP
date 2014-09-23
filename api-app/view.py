5481628aaa741fd08a3b8064dfb42facf78ddef0
kbeazer <kbeazer@fullsail.edu>
2014-09-23 18:38:11 -0400

    images added to update function

--- api-app/view.py
+++ api-app/view.py
@@ -14,12 +14,59 @@ class WunderView(object):
             self.__content += "  4-DAY FORECAST" + "<br />"
             self.__content += obj.current + "<br />"
             self.__content += obj.curmess + "<br />"
+
+            if obj.curimg == "sunny":
+                self.__content += '<img src="images/sunny.png />'
+            elif obj.curimg == "cloudy":
+                self.__content += '<img src="images/cloudy.png />'
+            elif obj.curimg == "chancetstorms":
+                self.__content += '<img src="images/chancetstorms.png />'
+            elif obj.curimg == "tstorms":
+                self.__content += '<img src="images/tstorms.png />'
+            else:
+                pass
+
             self.__content += obj.day1 + "<br />"
             self.__content += obj.d1mess + "<br />"
+
+            if obj.d1img == "sunny":
+                self.__content += '<img src="images/sunny.png />'
+            elif obj.d1img == "cloudy":
+                self.__content += '<img src="images/cloudy.png />'
+            elif obj.d1img == "chancetstorms":
+                self.__content += '<img src="images/chancetstorms.png />'
+            elif obj.d1img == "tstorms":
+                self.__content += '<img src="images/tstorms.png />'
+            else:
+                pass
+
             self.__content += obj.day2 + "<br />"
             self.__content += obj.d2mess + "<br />"
+
+            if obj.d2img == "sunny":
+                self.__content += '<img src="images/sunny.png />'
+            elif obj.d2img == "cloudy":
+                self.__content += '<img src="images/cloudy.png />'
+            elif obj.d2img == "chancetstorms":
+                self.__content += '<img src="images/chancetstorms.png />'
+            elif obj.d2img == "tstorms":
+                self.__content += '<img src="images/tstorms.png />'
+            else:
+                pass
+
             self.__content += obj.day3 + "<br />"
             self.__content += obj.d3mess + "<br />"
+
+            if obj.d3img == "sunny":
+                self.__content += '<img src="images/sunny.png />'
+            elif obj.d3img == "cloudy":
+                self.__content += '<img src="images/cloudy.png />'
+            elif obj.d3img == "chancetstorms":
+                self.__content += '<img src="images/chancetstorms.png />'
+            elif obj.d3img == "tstorms":
+                self.__content += '<img src="images/tstorms.png />'
+            else:
+                pass
 
     @property
     def content(self):
@@ -35,4 +82,3 @@ class WunderView(object):
         self.update()
 
 
-class ImageInput(object):
